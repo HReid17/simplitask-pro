5755dcf01212c89a825363c81efb7dbfbf96f1bd
Harrison Reid <H.Reid17@outlook.com>
2026-03-11 00:20:07 +0000

    test: add component tests for auth, dashboard, tasks, projects and calendar pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: "jsdom",  // simulates a browser
+    globals: true, // Enables test, expect, etc without importing
+    setupFiles: "./src/tests/setup.js" // runs a setup file before tests
+  }
 })
